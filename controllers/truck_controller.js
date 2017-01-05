d2be354bb11fee4fb604e2d5e3bf604f5f543b86
Michelle Luo <mluo0301@gmail.com>
2017-01-04 23:45:42 -0800

    intital set up

---
+++ controllers/truck_controller.js
@@ -0,0 +1,144 @@
+var express = require('express');
+var router = express.Router();
+// var pwUtil = require('../helpers/password');
+var bcrypt = require('bcryptjs');
+var User = require('../models').Truck;
+
+//this is the users_controller.js file
+router.get('/signup-signin', function(req,res) {
+	res.render('trucks/signup-signin', {
+		layout: 'main-registration'
+	});
+});
+
+
+//http://localhost:3000/trucks/truck
+router.get('/truck', function(req,res) {
+  res.render('trucks/truck', {
+    layout: 'main-registration'
+  });
+});
+
+router.get('/index', function(req,res) {
+  res.render('trucks/index', {
+    layout: 'main-registration'
+  });
+});
+
+router.get('/test', function(req,res) {
+  res.render('trucks/test', {
+    layout: 'main-registration'
+  });
+});
+
+router.get('/sign-up', function(req,res) {
+  res.render('trucks/test', {
+    layout: 'main-registration'
+  });
+});
+
+
+
+router.post("/sign-up", function(req, res) {
+  User.find({
+    email: req.body.email
+  }, function(err, users) {
+    if (users.length > 0){
+      console.log(users)
+      res.send('we already have an email or username for this account')
+    }else{
+
+      bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(req.body.password, salt, function(err, hash) {
+      
+      var truck = new User({
+        username: req.body.username,
+        password_hash: hash,
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+
+      });
+
+      truck.save(function(err) {
+
+        console.log("truck is trying to save11111")
+
+        if(err) throw err;
+
+        req.session.logged_in = true;
+        // the username to the session
+        req.session.username = truck.username;
+        // and the user's email.
+        req.session.user_email = truck.email;
+
+        req.session.firstName = truck.firstName;
+
+        req.session.lastName = truck.lastName;
+
+
+        res.render('trucks/index', {
+          email: req.session.user_email,
+          logged_in: req.session.logged_in,
+          username: req.session.username,
+          firstName: req.session.firstName,
+          lastName: req.session.lastName
+
+            });
+          });
+        });
+      });
+    };
+  });
+});
+
+
+router.post("/sign-in", function(req, res) {
+  User.find({email: req.body.email}, function(err, users) {
+    if(err) throw err;
+    if(users.length > 1) throw new Error("More than one user with the same name!");
+
+    var user = users[0];
+    user.validatePassword(req.body.password, function(err, success) {
+      if(err) throw err;
+          if(err) throw new Error("Something is wrong with your account.");
+
+      if(success) {
+
+        req.session.logged_in = true;
+        // the username to the session
+        req.session.username = user.username;
+        // and the user's email.
+        req.session.user_email = user.email;
+
+        req.session.firstName = user.firstName;
+
+        req.session.lastName = user.lastName;
+
+        res.render('trucks/index', {
+          email: req.session.user_email,
+          logged_in: req.session.logged_in,
+          username: req.session.username,
+          firstName: req.session.firstName,
+          lastName: req.session.lastName
+        });
+      } else {
+        req.session.logged_in = false;
+        res.redirect("index");
+      }
+    });
+  });
+});
+
+router.get("/sign-out", function(req,res) {
+ var username = req.session.username
+  req.session.destroy(function(err) {
+      User.update({username: username}, {loggedin:false}, 
+    function(err, num) {
+        console.log("loggedout "+username);
+    });
+     res.redirect("/")
+  })
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,124 @@
+// Include Server Dependencies
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session'); 
+var methodOverride = require('method-override');
+var bparser = require('body-parser');
+
+
+// Model controllers (rather than routes)
+var application_controller = require('./controllers/application_controller');
+var character_controller = require('./controllers/character_controller');
+var user_controller = require('./controllers/user_controller');
+var truck_controller = require('./controllers/truck_controller');
+
+app.use('/', application_controller);
+app.use('/characters', character_controller);
+app.use('/users', user_controller);
+app.use('/trucks', truck_controller);          
+
+//Require Schemas
+var Article = require('./server/model.js');
+
+// Create Instance of Express
+var app = express();
+var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
+
+// Run Morgan for Logging
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+app.use(express.static('./public'));
+
+// Override POST to have DELETE and PUT
+app.use(methodOverride('_method'))
+
+// Allow sessions
+app.use(session({ secret: 'app', cookie: { maxAge: null }}));
+app.use(cookieParser());
+
+// -------------------------------------------------
+
+// MongoDB Configuration configuration
+mongoose.connect('mongodb://luomichelle:Xigua123456@ds113628.mlab.com:13628/teamtow');
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+	console.log('Mongoose Error: ', err);
+});
+
+db.once('open', function () {
+	console.log('Mongoose connection successful.');
+});
+
+
+
+// -------------------------------------------------
+
+// Main Route
+app.get('/', function(req, res){
+	res.sendFile('./public/index.html');
+})
+
+// Route to get all saved articles
+app.get('/api/saved', function(req, res) {
+
+	Article.find({})
+		.exec(function(err, doc){
+
+			if(err){
+				console.log(err);
+			}
+			else {
+				res.send(doc);
+			}
+		})
+});
+
+// Route to add an article to saved list
+app.post('/api/saved', function(req, res){
+	var newArticle = new Article(req.body);
+
+	console.log(req.body)
+
+	var title = req.body.title;
+	var date = req.body.date;
+	var url = req.body.url;
+
+	newArticle.save(function(err, doc){
+		if(err){
+			console.log(err);
+		} else {
+			res.send(doc._id);
+		}
+	});
+});
+
+// Route to delete an article from saved list
+app.delete('/api/saved/', function(req, res){
+
+	var url = req.param('url');
+
+	Article.find({"url": url}).remove().exec(function(err, data){
+		if(err){
+			console.log(err);
+		}
+		else {
+			res.send("Deleted");
+		}
+	});
+});
+
+
+// -------------------------------------------------
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
